6d444200c8d4301a9f8ac685f3c3534205e822a5
Carlo <caruiz@mediamath.com>
2017-07-26 16:00:15 -0400

    need to refactor sync mech, unecessary data, issues when reaching 3800

--- reader3.py
+++ reader3.py
@@ -2,20 +2,54 @@ import mmap
 import os
 import struct
 import time
+import signal
+import sys
+
+MAP_SIZE = 4096 #4194304
+METAMAP_SIZE = 512
 
 time.sleep(0.5)
-fd = os.open('map', os.O_RDONLY)
-buf = mmap.mmap(fd, mmap.PAGESIZE, mmap.MAP_SHARED, mmap.PROT_READ)
+flag = 1
 
-new = prev  = 0
-while new < 1008:
-    while new == prev:
-        new  = int.from_bytes(buf[:8], byteorder='big')
-        
-    print('I have offset = %d' % new)
+def signal_handler(signal, frame):
+    flag = 0
+
+signal.signal(signal.SIGINT, signal_handler)
+
+# small IPC map
+fd = os.open('metaMap', os.O_RDONLY)
+buf = mmap.mmap(fd, METAMAP_SIZE, mmap.MAP_SHARED, mmap.PROT_READ)
+
+# larger output file
+fd2 = os.open('sharedMap', os.O_RDONLY)
+f = mmap.mmap(fd2, MAP_SIZE, mmap.MAP_SHARED, mmap.PROT_READ)
+
+new = prev = 0
+while flag:
+    while new == prev and flag:
+        new = int.from_bytes(buf[:8], byteorder='big')
+    
+    if not flag:
+        break
+
+
+    line = f.readline()
+    print(line)
+    try:
+        assert(prev + len(line) == new)
+    except AssertionError:
+        print('prev + len(line) = %d != %d = new' % ((prev + len(line)), new))
+       # sys.exit(1)
+    
     prev = new
+    if new > 3800:
+        f.seek(0)
+        prev = new = 0
+
 
 buf.close()
+f.close()
+os.close(fd2)
 os.close(fd)
     
 
--- writer3.py
+++ writer3.py
@@ -3,17 +3,39 @@ import mmap
 import os
 import struct
 import time
+import signal
 
-fd = os.open('map', os.O_CREAT | os.O_TRUNC | os.O_RDWR)
-assert(os.write(fd, bytearray(mmap.PAGESIZE)) == mmap.PAGESIZE)
-buf = mmap.mmap(fd, mmap.PAGESIZE, mmap.MAP_SHARED, mmap.PROT_WRITE)
+MAP_SIZE = 4096 #4194304
+METAMAP_SIZE = 512
+
+flag = 1
+def signal_handler(signal, frame):
+    flag = 0
+
+signal.signal(signal.SIGINT, signal_handler)
+
+# the big, output map
+fd = os.open('sharedMap', os.O_RDWR)
+f = mmap.mmap(fd, MAP_SIZE, mmap.MAP_SHARED, mmap.PROT_WRITE)
+
+# the small IPC map with metadata
+fd2 = os.open('metaMap', os.O_RDWR)
+buf = mmap.mmap(fd2, METAMAP_SIZE, mmap.MAP_SHARED, mmap.PROT_WRITE)
+
 
 offset = 0
-while offset < 1024:
+while flag:
     buf.seek(0)
     buf.write(offset.to_bytes(8, byteorder='big'))
-    offset+=16
+    string = "offset currently at %d\n" % offset
+    n = f.write(string.encode(encoding='ascii'))
+    offset += n
+    if offset > 3800:
+        f.seek(0)
+        offset = 0
     time.sleep(0.1)
 
 buf.close()
+f.close()
 os.close(fd) 
+os.close(fd2)
